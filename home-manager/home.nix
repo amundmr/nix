c74f3ad5fdd055f3ea225aa00c13b78cfc8dfd57
amundmr <amund.raniseth@gmail.com>
2024-09-17 07:30:44 +0200

    init

---
+++ home-manager/home.nix
@@ -0,0 +1,40 @@
+{ config, pkgs, ... }:
+
+{
+  nixpkgs.config.allowUnfree = true; # Required by 1password
+  home.username = "amund";
+  home.homeDirectory = "/home/amund";
+  home.stateVersion = "23.05";
+  home.packages = with pkgs; [
+    git
+    neovim
+    signal-desktop
+    _1password-gui
+    # Resource monitor
+    gnomeExtensions.vitals # dep on gtop & lm_sensors
+    gtop
+    lm_sensors
+  ];
+  programs.home-manager.enable = true;
+  dconf.settings = {
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+      enabled-extensions = with pkgs.gnomeExtensions; [
+        vitals.extensionUuid
+      ];
+    };
+    "org/gnome/shell/extensions/vitals" = {
+      hot-sensors = [
+      "_memory_usage_"
+      "_processor_usage_"
+      "__network-rx_max__"
+      "__network-tx_max__"
+      ];
+      show-temperature = false;
+      show-voltage = false;
+      show-fan = false;
+      show-system = false;
+      show-storage = false;
+    };
+  };
+}
---
+++ readme.md
@@ -0,0 +1,24 @@
+Followed [this](https://tech.aufomm.com/my-nix-journey-use-nix-with-ubuntu/) guide.
+
+# Update nix after changing configuration
+
+```bash
+home-manager switch --flake /home/amund/nix/#$USER
+```
+
+# Update packages in flake
+
+Run this command from inside ~/nix/
+
+```bash
+nix flake update
+```
+
+Remember to re-run the nix update command above to apply changes
+
+# Cleaning
+
+```bash
+nix store gc
+```
+
