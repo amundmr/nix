3b8820fa38519d1a8d6dd8b8a6b671338d7d2a3c
amundmr <amund.raniseth@gmail.com>
2025-01-10 16:41:45 +0100

    can't get it to work :(

--- home-manager/systemd_timers/google_drive_rsync.nix
+++ home-manager/systemd_timers/google_drive_rsync.nix
@@ -1,25 +1,29 @@
 # This timer runs every hour to back up local work to google drive.
-systemd.user.services = {
-  backup_google_drive = {
-    Unit = {
-      Description = "Backup to Google Drive";
-    };
-    Service = {
-      Type = "oneshot";
-      ExecStart = "/home/amund/nix/home-manager/systemd_timers/rclone_copy.sh";
-  );
-    };
-    Install.WantedBy = [ "default.target" ];
-  };
-};
+{config, pkgs, ...}:
 
-systemd.user.timers = {
-  backup_google_drive = {
-    Unit.Description = "Hourly timer for Backup to Google Drive";
-    Timer = {
-      Unit = "backup_google_drive";
-      OnCalendar = "*-*-* *:20:00";
+{
+  systemd.user.services = {
+    backup_google_drive = {
+      Unit = {
+        Description = "Backup to Google Drive";
+      };
+      Service = {
+        Type = "oneshot";
+        ExecStart = "/home/amund/nix/home-manager/systemd_timers/rclone_copy.sh";
+	StandardOutput = "journal";
+	StandardError = "journal";
+      };
+      Install.WantedBy = [ "default.target" ];
     };
-    Install.WantedBy = [ "timers.target" ];
   };
-};
+
+  systemd.user.timers = {
+    backup_google_drive = {
+      Unit.Description = "Hourly timer for Backup to Google Drive";
+      Timer = {
+        OnCalendar = "*-*-* *:40:00";
+      };
+      Install.WantedBy = [ "timers.target" ];
+    };
+  };
+}
